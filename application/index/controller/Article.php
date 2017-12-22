d22c7e80052982299a13e4719c6beba1d1beba41
DarkKris <long4664030@163.com>
2017-12-22 17:51:37 +0800

    new.html基本完成

--- application/index/controller/Article.php
+++ application/index/controller/Article.php
@@ -11,7 +11,7 @@ use think\Controller;
 
 class Article extends Controller
 {
-    public function new()
+    public function anew()
     {
         $taglist = ['0','2'];
         $this->assign('taglist',$taglist);
@@ -31,6 +31,36 @@ class Article extends Controller
     public function create()
     {
 
+    }
+
+    public function achange()
+    {
+
+    }
+
+    public function read_all ()
+    {
+        $dir = '/Users/wdhhdzyhb/darkkris.github.io/_posts';
+        $ignore = '.DS_Store';
+        if(!is_dir($dir)) return false;
+
+        $handle = opendir($dir);
+
+        if($handle){
+            while(($fl = readdir($handle)) !== false){
+                $temp = $dir.DIRECTORY_SEPARATOR.$fl;
+//                如果不加  $fl!='.' && $fl != '..'  则会造成把$dir的父级目录也读取出来
+//                if(is_dir($temp) && $fl!='.' && $fl != '..'){
+//                    echo '目录：'.$temp.'<br>';
+//                    read_all($temp);
+//                }else{
+                if($fl!='.' && $fl != '..' && $fl != $ignore)
+                {
+                    echo '文件：'.$temp.'<br>';
+                }
+//                }
+            }
+        }
     }
 }
 ?>
